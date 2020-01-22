56be8354c943bf8f170496674e5f7e7b853a45dc
russellpurvis <pur17007@byui.edu>
2020-01-22 09:54:30 -0700

    last date modified

--- scripts/currentDate.js
+++ scripts/currentDate.js
@@ -3,4 +3,7 @@ console.log(currentDate);
 
 const currentYear = currentDate.getFullYear();
 console.log(currentYear);
-document.getElementById("currentYear").innerHTML = currentYear;
+document.getElementById("current-year").innerHTML = currentYear;
+
+document.getElementById("last-modified").innerHTML = document.lastModified;
+
