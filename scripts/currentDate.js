f1c44b9de85cc066b9a9e581a9eb3b8ff556478c
russellpurvis <38728527+russellpurvis@users.noreply.github.com>
2020-01-24 10:19:22 -0700

    Delete currentDate.js

--- scripts/currentDate.js
+++
@@ -1,9 +0,0 @@
-const currentDate = new Date();
-console.log(currentDate);
-
-const currentYear = currentDate.getFullYear();
-console.log(currentYear);
-document.getElementById("current-year").innerHTML = currentYear;
-
-document.getElementById("last-modified").innerHTML = document.lastModified;
-
