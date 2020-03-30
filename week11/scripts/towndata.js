018df1630e45542c8914bdd64037fe9c432d99ff
russellpurvis <pur17007@byui.edu>
2020-03-30 16:41:07 -0600

    events

---
+++ week11/scripts/towndata.js
@@ -0,0 +1,23 @@
+function getEvents(cityName){
+    fetch('https://byui-cit230.github.io/weather/data/towndata.json')
+        .then(result => result.json())
+        .then(
+            (result) => {
+                result.towns.forEach(
+                    town => {
+                        if (town.name.toLowerCase() === cityName.toLowerCase()) {
+                            town.events.forEach(
+                                event => {
+                                    let li = document.createElement('li');
+                                    li.textContent = event;
+    
+                                    document.querySelector('#events').appendChild(li);
+                                }
+                            )
+                        }
+                    }
+    
+                )
+            }
+        );
+    }
